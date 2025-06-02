1d49e14784bc96642a5412b106ceee0cf8582a1b
Rafael da Cunha <136820942+rafaelcunhaa@users.noreply.github.com>
2025-06-02 20:43:48 -0300

    Update deadlock_handler.py

--- M2/deadlock_handler.py
+++ M2/deadlock_handler.py
@@ -1,21 +1,18 @@
-def try_lock_with_wait_die(transaction, resource):
-    """
-    Tenta adquirir o bloqueio do recurso. Se não conseguir, verifica se deve abortar a transação.
-    """
+def try_lock_with_wait_die(transacao, resource):
     if resource.lock.acquire(blocking=False):
-        resource.locked_by = transaction.tid
-        print(f"[T{transaction.tid}] Adquiriu recurso {resource.item_id}")
+        resource.locked_by = transacao
+        print(f"[T{transacao.tid}] Bloqueou recurso {resource.item_id}")
         return True
     else:
-        other = resource
-        print(f"[T{transaction.tid}] Esperando recurso {resource.item_id} bloqueado por T{other.locked_by}. Abortando transação.")
+        other = resource.locked_by
+        print(f"[T{transacao.tid}] Esperando recurso {resource.item_id} (em posse de T{other.tid})")
 
-       if transaction.timestamp < other.locked_by.timestamp:
-            print(f"[T{transaction.tid}] Espera permitida (mais velha que T{other.tip})")
+        if transacao.timestamp < other.timestamp:
+            print(f"[T{transacao.tid}] Espera permitida (mais velha que T{other.tid})")
             while resource.lock.locked():
-                pass
-            return try_lock_with_wait_die(transaction, resource)
+                pass  # espera ocupada (poderia ser melhor com condition variable)
+            return try_lock_with_wait_die(transacao, resource)
         else:
-         print(f"[T{transacao.tid}] Abortada (nais nova que T{other.locked_by})")
-        transaction.abort() = True
-        return False
+            print(f"[T{transacao.tid}] Abortada (mais nova que T{other.tid}) - Wait-die")
+            transacao.aborted = True
+            return False
