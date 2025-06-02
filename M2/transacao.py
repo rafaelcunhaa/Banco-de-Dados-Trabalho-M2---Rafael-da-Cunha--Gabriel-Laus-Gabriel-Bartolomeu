6c27d527cffc17c52cfb6aa5719169132f677d2c
Rafael da Cunha <136820942+rafaelcunhaa@users.noreply.github.com>
2025-06-02 20:44:57 -0300

    Update transacao.py

--- M2/transacao.py
+++ M2/transacao.py
@@ -3,10 +3,8 @@ import time
 import random
 from deadlock_handler import try_lock_with_wait_die
 
-
-
 class Transacao(threading.Thread):
-    def __int__(self, tid, timestamp, resource_x, resource_y):
+    def __init__(self, tid, timestamp, resource_x, resource_y, manager):
         super().__init__()
         self.tid = tid
         self.timestamp = timestamp
@@ -16,40 +14,41 @@ class Transacao(threading.Thread):
         self.aborted = False
 
     def run(self):
-        print(f"[T{self.tip}] Iniciando execução (timestamp: {self.timestamp})")
-        self.random_wait()
+        while not self.aborted:
+            print(f"[T{self.tid}] Iniciando execução (timestamp: {self.timestamp})")
+            self.random_wait()
 
-        if not try_lock_with_wait_die(self, self.resource_x):
-            self.restart()
-            continue
+            if not try_lock_with_wait_die(self, self.resource_x):
+                self.restart()
+                continue
 
-        self.random_wait()
+            self.random_wait()
 
-        if not try_lock_with_wait_die(self, self.resource_y):
+            if not try_lock_with_wait_die(self, self.resource_y):
+                self.resource_x.lock.release()
+                self.resource_x.locked_by = None
+                print(f"[T{self.tid}] Liberou recurso {self.resource_x.item_id}")
+                self.restart()
+                continue
+
+            self.random_wait()
+
             self.resource_x.lock.release()
             self.resource_x.locked_by = None
             print(f"[T{self.tid}] Liberou recurso {self.resource_x.item_id}")
-            continue
+            self.random_wait()
 
-        self.random_wait()
+            self.resource_y.lock.release()
+            self.resource_y.locked_by = None
+            print(f"[T{self.tid}] Liberou recurso {self.resource_y.item_id}")
+            self.random_wait()
 
-        self.resource_x.lock.release()
-        self.resource_x.locked_by = None
-        print(f"[T{self.tid}] Liberou recurso {self.resource_x.item_id}")
-        self.random_wait()
-
-        self.resource_y.lock.release()
-        self.resource_y.locked_by = None
-        print(f"[T{self.tid}] Liberou recurso {self.resource_y.item_id}")
-        self.random_wait()
-
-        print(f"[T{self.tid}] Finalizando execução")
-        break
+            print(f"[T{self.tid}] Finalizou com sucesso")
+            break
 
     def restart(self):
-        print(f"[T{self.tid}] Abortada por deadlock")
+        print(f"[T{self.tid}] Abortada por deadlock - Reiniciando...")
         self.random_wait()
 
     def random_wait(self):
-        time.sleep(random.uniform(0.1, 0.5))  # Espera aleatória entre 100ms e 500ms
-
+        time.sleep(random.uniform(0.1, 0.5))
