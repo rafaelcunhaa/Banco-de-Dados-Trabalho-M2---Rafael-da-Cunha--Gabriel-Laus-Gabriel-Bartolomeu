0ea1721b2552d75e8d1d6f01296575291bcca5e1
Rafael da Cunha <136820942+rafaelcunhaa@users.noreply.github.com>
2025-06-18 09:44:05 -0300

    Update resources.py

--- M2/resources.py
+++ M2/resources.py
@@ -2,7 +2,7 @@ import threading
 
 class Resource:
     def __init__(self, item_id):
-        self.item_id = item_id
-        self.lock = threading.Lock()
-        self.locked_by = None
-        self.waiting_queue = []  # usado futuramente se precisar
+        self.item_id = item_id  # Nome do recurso (ex: "X" ou "Y")
+        self.lock = threading.Lock()  # Lock associado ao recurso
+        self.locked_by = None  # Indica qual transação está usando o recurso
+        self.waiting_queue = []  # Lista de espera (não usada aqui, mas pode ser útil futuramente)
